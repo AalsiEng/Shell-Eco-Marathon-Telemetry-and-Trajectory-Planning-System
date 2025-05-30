601fed684b5f8fda71b8a5340bfc14ab70d3d75a
AalsiEng <alamgiriandon@gmail.com>
2025-05-31 00:34:08 +0500

    completed csv reading of torque and power curve alongwith use of those values in acceleration calculations

--- physics_prototype.py
+++ physics_prototype.py
@@ -1,5 +1,8 @@
 import numpy as np
+import math
 from math import sqrt
+import csv
+import pandas as pd
 tire_coeff = 0.2 #assumed value for friciton coeffecient of tire rubber
 g = 9.81
 prim_red = 4.055
@@ -23,6 +26,35 @@ drag_force = 0.0
 frontal_area = 1.95
 drag_coeff = 0.37
 mass = 190
+df = pd.read_csv('125_power_curve.csv')
+hp = 0
+torque = 0
+rpm = 0
+wheel_radius = 0.46
+accel = 0.0
+neg_acceleration = 0.0
+total_accel = 0.0
+time_step = 0.1  # seconds
+
+def get_hp(rpm):
+    global df
+    hp = df.loc[df['rpm'] == rpm, 'hp']
+    if not hp.empty:
+        print(f"HP: {hp.values[0]}")
+        return hp.values[0]
+    else:
+        print("RPM value not found.")
+        return None
+    
+def get_torque(rpm):
+    global df
+    torque = df.loc[df['rpm'] == rpm, 'torque']
+    if not torque.empty:
+        print(f"Torque: {torque.values[0]}")
+        return torque.values[0]
+    else:
+        print("RPM value not found.")
+        return None
 
 
 def idle_check(rpm):
@@ -80,28 +112,54 @@ def speed_calc(rpm):
     speed_kmh = init_speed_ms * 3.6  # Convert m/s to km/h
     print(f"Speed: {speed_kmh:.2f} km/h")
 
-def friction_calculation():
+def deceleration_calculation():
     global tire_coeff, g, init_speed_ms, speed_kmh
-    global final_speed_ms, drag_accel    
+    global final_speed_ms, drag_accel, neg_acceleration    
     neg_acceleration = -tire_coeff * g - drag_accel
-    distance = (init_speed_ms ** 2) / (2 * abs(neg_acceleration))
-    speed_kmh = init_speed_ms * 3.6  # Convert m/s to km/h
-    final_speed_ms = sqrt(init_speed_ms ** 2 + 2 * neg_acceleration * distance)
-    print(f"Speed: {speed_kmh:.2f} km/h")
-    print(f"Distance to stop: {distance:.2f} m")
 
+
+def acceleration_calculation():
+    global hp, wheel_radius, mass, accel, rpm
+    calculated_torque = (hp * 5252) / rpm  # Convert HP to torque
+    accel = calculated_torque / (wheel_radius * mass)  # Calculate acceleration in m/s^2
+
+def accel_total():
+    global accel, neg_acceleration, total_accel
+    total_accel = accel + neg_acceleration
+    
+def speed_update():
+    global init_speed_ms, final_speed_ms, total_accel, time_step
+    final_speed_ms = init_speed_ms + total_accel * time_step
+    if final_speed_ms < 0:
+        final_speed_ms = 0
+
+def distance_per_time():
+    global init_speed_ms, final_speed_ms, time_step
+    distance = (init_speed_ms + final_speed_ms) / 2 * time_step
+    return distance
 
 
 def main():
+    global hp, torque
     global rpm, init_speed_ms, final_speed_ms, has_started, on_idle
     rpm = int(input("Enter RPM: "))
+    hp = int(get_hp(rpm))
+    torque = int(get_torque(rpm))
     idle_check(rpm)
     begin_check(on_idle)
     init_speed_ms = final_speed_ms 
     if has_started == True:
         speed_calc(rpm)
         drag_effect()
-        friction_calculation()
+        deceleration_calculation()
+        acceleration_calculation()
+        accel_total()
+        speed_update()
+        distance = distance_per_time()
+        print(f"Distance traveled in this time step: {distance:.2f} meters")
+    #    speed_calc(rpm)
+    #    drag_effect()
+    #    friction_calculation()
 
 main()
 
